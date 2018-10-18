e5c56e3b2fb81682fa14a12585c48c294cc1fdb9
Brandon Rush <git@brandonrush.net>
2018-10-17 22:13:10 -0400

    Fixed default test

--- src/app/components/App.test.js
+++ src/app/components/App.test.js
@@ -1,10 +1,14 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
 
 import App from './App';
 
-it('renders without crashing', () => {
+it('Renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
